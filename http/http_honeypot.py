fc131a325ac30746061fb1799eb9e8b10d741f45
Zachary Cotton <mr.zacharycotton@gmail.com>
2023-01-14 13:22:42 -0800

    .env and Log Client Ip

--- http/http_honeypot.py
+++ http/http_honeypot.py
@@ -1,5 +1,5 @@
-import flask, os, logging
-from flask import Flask, request
+import flask, os, logging, base64
+from flask import Flask, request, Response
 app = Flask(__name__)
 
 wd = os.environ.get('WORKING_DIRECTORY') or os.getcwd()
@@ -7,8 +7,16 @@ wd = os.environ.get('WORKING_DIRECTORY') or os.getcwd()
 log = open(f'{wd}/http.log', 'a')
 logging.basicConfig(level=logging.DEBUG, stream=log, format='[%(asctime)s] %(message)s')
 
+laravel6_env_data = base64.b64decode(open(f'{wd}/data/laravel6.env.b64').read())
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    logging.info(f'{request.remote_addr} - {request.method} {request.path}\n{request.headers}\n{request.data}')
+    logging.info(f'{request.headers.get("X-Remote-IP")} - {request.method} {request.path}\n{request.headers}\n{request.data}')
+    
+    if request.path == '/.env':
+        resp = Response(laravel6_env_data)
+        resp.headers['Content-Type'] = 'text/plain'
+        return resp
+
     return '404 File not found.', 404
