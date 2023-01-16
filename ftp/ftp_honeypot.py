ea6b14ce2a3c94f155ec61497bf4dc3a89c16df2
Zachary Cotton <mr.zacharycotton@gmail.com>
2023-01-16 13:42:22 -0800

    Structured Logging for Ftp

--- ftp/ftp_honeypot.py
+++ ftp/ftp_honeypot.py
@@ -16,44 +16,44 @@ if pid > 0:
     s.close()
     sys.exit(0)
 
-import logging
 from twisted.internet import protocol, reactor, endpoints
+from log_ import Logger
 
 os.setgid(1000)
 os.setuid(1000)
 
-log = open(f'{wd}/ftp.log', 'a')
-logging.basicConfig(level=logging.DEBUG, stream=log, format='[%(asctime)s] %(message)s')
-
 class FtpProtocol(protocol.Protocol):
 
     def __init__(self):
-        self.username = ''
+        self.logger = Logger('ftp')
 
     def connectionMade(self):
+        self.logger.bind(ip=self.transport.getPeer().host)
         self.transport.write(b'220 Welcome to the DLP Test FTP Server\r\n')
 
     def dataReceived(self, data):
         try:
             data = data.decode().strip()
         except:
-            logging.exception(f'Error parsing the following command {data}: ')
+            self.logger.exception(message='Error parsing command.', action='command', command=str(data))
 
-        logging.info(f'{self.transport.getPeer().host}: > {data}')
+        self.logger.info(action='command', command=data)
 
         parts = data.split(' ')
         cmd = parts[0]
         
         if cmd == 'USER':
-            self.username = parts[1]
+            self.logger.bind(username=parts[1])
             self.transport.write(b'331 Please specify the password.\r\n')
         elif cmd == 'PASS':
+            self.logger.bind(password=parts[1])
+            self.logger.info(action='login')
             self.transport.write(b'230 Login successful.\r\n')
         elif cmd == 'SYST':
             self.transport.write(b'215 UNIX Type: L8\r\n')
         elif cmd == 'CLOSE' or cmd == 'QUIT' or cmd == 'EXIT':
             self.transport.write(b'221 Goodbye.\r\n')
-            self.transport.close()
+            self.transport.loseConnection()
         else:
             self.transport.write(b'500 Invalid command.\r\n')
         
