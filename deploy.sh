c495ec90a9a885b0b5695f60731a781c994a8cef
Zachary Cotton <mr.zacharycotton@gmail.com>
2023-01-27 23:12:46 -0800

    Ignore Known Hosts

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 . secrets.env
 
 command() {
-    ssh -i "$PEM_FILE" "$USERNAME@$HOST_IP" $1
+    ssh -o StrictHostKeychecking=no -i "$PEM_FILE" "$USERNAME@$HOST_IP" $1
 }
 
 for dir in `ls ./modules`; do
@@ -12,12 +12,12 @@ for dir in `ls ./modules`; do
     cd "../.." && \
     command "sudo rm -rf /usr/local/bin/${dir}_honeypot" && \
     command "rm -rf ~/staging/${dir}" && \
-    scp -r -i "$PEM_FILE" "modules/${dir}" "$USERNAME@$HOST_IP:~/staging" && \
+    scp -o StrictHostKeychecking=no -r -i "$PEM_FILE" "modules/${dir}" "$USERNAME@$HOST_IP:~/staging" && \
     command "sudo mv ~/staging/${dir} /usr/local/bin" && \
     command "sudo mv /usr/local/bin/${dir} /usr/local/bin/${dir}_honeypot" && \
     command "sudo chmod a+x /usr/local/bin/${dir}_honeypot"
 
-    scp -r -i "$PEM_FILE" "openrc/basic.tmp.sh" "$USERNAME@$HOST_IP:~/staging/${dir}_honeypot.sh" && \
+    scp -o StrictHostKeychecking=no -r -i "$PEM_FILE" "openrc/basic.tmp.sh" "$USERNAME@$HOST_IP:~/staging/${dir}_honeypot.sh" && \
     command "sudo mv staging/${dir}_honeypot.sh /etc/init.d/${dir}_honeypot" && \
     command "sudo chmod a+rx /etc/init.d/${dir}_honeypot" && \
     command "sudo rc-update add ${dir}_honeypot default" && \
