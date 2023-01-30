55ec47e860fd74060a4eaf4f1cd23f1c1dbbb66e
Zachary Cotton <mr.zacharycotton@gmail.com>
2023-01-29 17:56:15 -0800

    Fix Deploy

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,10 @@ for dir in `ls ./services`; do
     command "rm -rf ~/staging/${dir}" && \
     scp -o StrictHostKeychecking=no -r -i "$PEM_FILE" "services/${dir}" "$USERNAME@$HOST_IP:~/staging" && \
     command "sudo mv ~/staging/${dir} /usr/local/bin" && \
-    command "sudo mv /usr/local/bin/${dir} /usr/local/bin/${dir}" && \
     command "sudo chmod a+x /usr/local/bin/${dir}/${dir}"
     command "sudo setcap cap_net_bind_service=+ep /usr/local/bin/${dir}/${dir}"
 
-    scp -o StrictHostKeychecking=no -r -i "$PEM_FILE" "openrc/basic.tmp.sh" "$USERNAME@$HOST_IP:~/staging/${dir}_honeypot.sh" && \
+    scp -o StrictHostKeychecking=no -r -i "$PEM_FILE" "openrc/basic.tmp.sh" "$USERNAME@$HOST_IP:~/staging/${dir}.sh" && \
     command "sudo mv staging/${dir}.sh /etc/init.d/${dir}" && \
     command "sudo chmod a+rx /etc/init.d/${dir}" && \
     command "sudo rc-update add ${dir} default" && \
