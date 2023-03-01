3d13483841f223fab0a47116707e6860c97a20fa
Zachary Cotton <mr.zacharycotton@gmail.com>
2023-02-28 21:27:15 -0800

    Telnet Commands (#7)

--- services/telnet_honeypot/telnet_honeypot.go
+++ services/telnet_honeypot/telnet_honeypot.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"logging"
 	"net"
 	"os"
+	"regexp"
 	"reporting"
 	"strings"
 )
@@ -24,6 +28,9 @@ func (log *TelnetLog) IpAddress() string {
 
 var serviceName string = "telnet"
 var logger logging.Logger
+
+var procMounts []byte
+var elf []byte
 
 func handleRequest(conn net.Conn) {
 	state := "username"
@@ -77,11 +84,72 @@ func handleRequest(conn net.Conn) {
 				Ip:      data.IpAddress(),
 				Data:    data,
 			})
+
+			for _, command := range strings.Split(dataStr, ";") {
+				command = strings.Trim(command, " \t\n\r")
+
+				match, _ := regexp.MatchString(".*[A-Z]{5,6}.*", command)
+				if match {
+					exp, _ := regexp.Compile("[A-Z]{5,6}")
+					token := exp.FindString(command)
+					resp := fmt.Sprintf("%s: applet not found\r\n", token)
+					conn.Write([]byte(resp))
+				}
+
+				if strings.Contains(command, "/proc/mounts") {
+					conn.Write(procMounts)
+				}
+
+				match, _ = regexp.MatchString(".*echo -n?e '(\\\\x[0-9a-f]{2})+'", command)
+				if match {
+					exp, _ := regexp.Compile("\\\\x[0-9a-f]{2}")
+					for _, digit := range exp.FindAllString(command, -1) {
+						digit := strings.Replace(digit, "\\x", "", 1)
+						data, _ := hex.DecodeString(digit)
+						conn.Write(data)
+					}
+				}
+
+				if command == "tftp" {
+					conn.Write([]byte("tftp: applet not found\r\n"))
+				}
+
+				if command == "wget" {
+					conn.Write([]byte("wget: missing URL\nUsage: wget [OPTION]... [URL]...\n\nTry `wget --help' for more options.\r\n"))
+				}
+
+				if strings.Contains(command, "dd") {
+					conn.Write(elf)
+					conn.Write([]byte("\r\n"))
+				}
+
+				if strings.Contains(command, "exit") {
+					conn.Close()
+				}
+			}
 		}
 	}
 }
 
+func loadB64File(fname string) []byte {
+	dat, err := ioutil.ReadFile(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dat, err = base64.StdEncoding.DecodeString(string(dat))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return dat
+}
+
 func main() {
+
+	procMounts = loadB64File("./proc_mounts.b64")
+	elf = loadB64File("./elf.b64")
+
 	logger = logging.Logger{
 		Service: &serviceName,
 	}
